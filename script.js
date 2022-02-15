5db7b6a378026ac353f9e5e232fc73792afebff1
Alex Jordan <AlJord91@gmail.com>
2022-02-14 20:42:23 -0500

    starting to understand

---
+++ script.js
@@ -0,0 +1,39 @@
+const url = 'http://localhost:3000/notes'
+const notesOutput = document.getElementById('notesOutput')
+const notesForm = document.getElementById('notesForm')
+
+
+function myNotes() {
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+        for (let noteObj of data) {
+            renderNoteItem(noteObj)
+        }
+        })
+}
+
+notesForm.addEventListener('submit', function () {
+    event.preventDefault()
+    const noteText = document.querySelector('#note-text').value
+    fetch(url, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            item: noteText,
+    }),
+})
+    .then(r => r.json())
+    .then((data) => {
+        renderNoteItem(data)
+    })
+})
+
+function renderNoteItem(noteObj) {
+    const itemEl = document.createElement('p')
+    itemEl.p = noteObj.id
+    itemEl.innerHTML = `${noteObj.item}<br>${noteObj.body}`
+    notesOutput.appendChild(itemEl)
+}
+
+myNotes();
